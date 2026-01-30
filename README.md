1eced5fde9bc17ae972d6ddc73bcc0d8f1342801
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-01-30 15:33:11 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# smoke
+
+> Serverless customer support platform
+
+[![Deploy to Cloudflare](https://deploy.workers.cloudflare.com/button)](https://deploy.workers.cloudflare.com/?url=https://github.com/earth-app/smoke)
+
+---
---
+++ src/app.vue
@@ -0,0 +1,28 @@
+<template>
+	<UApp :toaster="{ expand: false }">
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</UApp>
+</template>
+
+<script setup lang="ts">
+useHead({
+	link: [
+		{
+			rel: 'icon',
+			type: 'image/x-icon',
+			href: '/favicon.ico'
+		},
+		{
+			rel: 'icon',
+			type: 'image/png',
+			href: '/favicon.png'
+		},
+		{
+			rel: 'apple-touch-icon',
+			href: '/favicon.png'
+		}
+	]
+});
+</script>
