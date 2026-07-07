15ee641c409d8d6e2ae183cfb98344bdad0a49d5
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-07-06 21:18:07 -0500

    feat: create app configuration

---
+++ src/app.config.ts
@@ -0,0 +1,15 @@
+export default defineAppConfig({
+	name: 'Smoke',
+	description: 'Serverless customer support',
+	ui: {
+		colors: {
+			primary: 'indigo',
+			neutral: 'slate'
+		},
+		button: {
+			defaultVariants: {
+				color: 'primary'
+			}
+		}
+	}
+});
