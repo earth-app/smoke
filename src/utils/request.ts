dfbcfa2a5bd66dcd70cb3e6acc872a1ad8c855fa
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-07-06 21:18:30 -0500

    chore: update request utiilities with more options

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -5,6 +5,10 @@ export type QueryParameters = {
 	offset?: number;
 	sort?: string;
 	sort_direction?: 'asc' | 'desc';
+	status?: string;
+	priority?: string;
+	labels?: string;
+	assignee?: string;
 };
 
 export function toSearchParams(options?: QueryParameters): URLSearchParams {
@@ -17,6 +21,10 @@ export function toSearchParams(options?: QueryParameters): URLSearchParams {
 	if (options.offset) params.append('offset', options.offset.toString());
 	if (options.sort) params.append('sort', options.sort);
 	if (options.sort_direction) params.append('sort_direction', options.sort_direction);
+	if (options.status) params.append('status', options.status);
+	if (options.priority) params.append('priority', options.priority);
+	if (options.labels) params.append('labels', options.labels);
+	if (options.assignee) params.append('assignee', options.assignee);
 
 	return params;
 }
