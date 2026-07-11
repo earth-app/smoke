4e9f04b394cae39f8cf05972834c356a9e02e982
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-07-11 18:05:54 -0500

    feat: create error.vue file

---
+++ src/error.vue
@@ -0,0 +1,28 @@
+<template>
+	<UApp>
+		<NuxtLayout>
+			<div class="flex flex-col items-center justify-center min-h-screen">
+				<UIcon
+					name="mdi:alert-circle"
+					class="text-red-500 size-12 md:size-14 lg:size-16"
+				/>
+				<h1 class="text-4xl font-bold mb-4">
+					{{ error?.statusText || 'Oops! Something went wrong.' }}
+				</h1>
+				<p class="text-lg mb-8">
+					{{ error?.message || "We're sorry, but we couldn't find the page you were looking for." }}
+				</p>
+				<UButton
+					class="hover:cursor-pointer"
+					@click="$router.push('/')"
+				>
+					Go Back Home
+				</UButton>
+			</div>
+		</NuxtLayout>
+	</UApp>
+</template>
+
+<script setup lang="ts">
+const error = useError();
+</script>
