95861725a6081f4fd612009aef39204c92222f5c
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-07-14 17:21:41 -0500

    chore: delete dead auth middleware

--- src/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to) => {
-	const { isAuthenticated, fetchUser } = useAuth();
-
-	await fetchUser();
-
-	if (!isAuthenticated.value) {
-		return navigateTo(`/login?redirect=${encodeURIComponent(to.fullPath)}`);
-	}
-});
